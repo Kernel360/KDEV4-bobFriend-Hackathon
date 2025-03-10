dabea44feefd9d95d1aa5b9232c5a69cbfae60db
ongsttt52 <ongsttt52@gmail.com>
2025-03-11 02:45:28 +0900

    feature: JWT 인증 구현 및 응답 객체 ResponsEntity로 변경 (#8)

--- src/main/java/com/hackathon/bobFriend/users/controller/UserController.java
+++ src/main/java/com/hackathon/bobFriend/users/controller/UserController.java
@@ -5,6 +5,7 @@ import com.hackathon.bobFriend.users.dto.UserResponse;
 import com.hackathon.bobFriend.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,32 +18,38 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public UserResponse createUser(@Valid @RequestBody UserRequest userRequest) {
-        return userService.createUser(userRequest);
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+
+        return ResponseEntity.ok(userService.createUser(userRequest));
     }
 
-    @PostMapping("/login")
-    public UserResponse login(@Valid @RequestBody UserRequest userRequest) {
-        return userService.login(userRequest);
+    @PostMapping("/auth")
+    public ResponseEntity<String> login(@Valid @RequestBody UserRequest userRequest) {
+
+        return ResponseEntity.ok(userService.login(userRequest));
     }
 
     @GetMapping("/users/{id}")
-    public UserResponse getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+
+        return  ResponseEntity.ok(userService.getUserById(id));
     }
 
     @GetMapping("/users")
-    public List<UserResponse> getAllUser() {
-        return userService.getAllUser();
+    public ResponseEntity<List<UserResponse>> getAllUser() {
+
+        return  ResponseEntity.ok(userService.getAllUser());
     }
 
     @PatchMapping("/users/{id}")
-    public UserResponse updateUser(@Valid @RequestBody UserRequest userRequest) {
-        return userService.updateUser(userRequest);
+    public ResponseEntity<UserResponse> updateUser(@Valid @RequestBody UserRequest userRequest) {
+
+        return  ResponseEntity.ok(userService.updateUser(userRequest));
     }
 
     @DeleteMapping("/users/{id}")
-    public UserResponse deleteUser(@PathVariable Long id) {
-        return userService.deleteUser(id);
+    public ResponseEntity<UserResponse> deleteUser(@PathVariable Long id) {
+
+        return ResponseEntity.ok(userService.deleteUser(id));
     }
 }
