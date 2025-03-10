4308da607d2fff8aa4bbc47a2b2c8f6e80592762
EOJIN <feshed528@gmail.com>
2025-03-10 23:34:41 +0900

    feature: 데이터베이스 초기화 및 더미 데이터 삽입 (MySQL: schema.sql, data.sql)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+--create database bobFriend;
+use bobFriend;
+
+DROP TABLE IF EXISTS gatherings;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS places;
+
+create table users (
+	id bigint auto_increment primary key,
+    name varchar(30) not null, /*realname*/
+    email varchar(50) not null unique,
+	password varchar(50) not null,
+    is_active boolean default true,
+    created_at timestamp default current_timestamp
+);
+
+create table places (
+	id bigint auto_increment primary key,
+	name varchar(50) unique,
+    content text,
+    address varchar(100) not null,
+    category enum('KOR', 'JPN', 'CHN', 'WES', 'ETC') not null
+);
+
+create table gatherings (
+	id bigint auto_increment primary key,
+	title varchar(100) not null,
+    content text not null,
+    gathering_at datetime not null,
+    closing_at datetime not null,
+    created_at timestamp default current_timestamp,
+    max_participant int not null,
+    state enum('ING', 'END') not null,
+    talk_thema varchar(100),
+    talk_flag enum('I', 'E', 'N') not null,
+	is_deleted boolean default false,
+    user_id bigint not null, /*fk from users table*/
+    place_id bigint, /*fk from places table*/
+    foreign key (user_id) references users (id),
+    foreign key (place_id) references places (id)
+);
+
+select * from users;
