b9a742ad26d2ffdbfcb6b2a55a4fd103af3cdd32
ongsttt52 <ongsttt52@gmail.com>
2025-03-11 00:29:48 +0900

    feat : User - Entity, DTO 클래스 작성 및 CRUD 구현 (#1)

--- src/main/java/com/hackathon/bobFriend/users/controller/UserController.java
+++ src/main/java/com/hackathon/bobFriend/users/controller/UserController.java
@@ -5,7 +5,6 @@ import com.hackathon.bobFriend.users.dto.UserResponse;
 import com.hackathon.bobFriend.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,38 +17,27 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
-
-        return ResponseEntity.ok(userService.createUser(userRequest));
-    }
-
-    @PostMapping("/auth")
-    public ResponseEntity<String> login(@Valid @RequestBody UserRequest userRequest) {
-
-        return ResponseEntity.ok(userService.login(userRequest));
+    public UserResponse createUser(@Valid @RequestBody UserRequest userRequest) {
+        return userService.createUser(userRequest);
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
-
-        return  ResponseEntity.ok(userService.getUserById(id));
+    public UserResponse getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
     @GetMapping("/users")
-    public ResponseEntity<List<UserResponse>> getAllUser() {
-
-        return  ResponseEntity.ok(userService.getAllUser());
+    public List<UserResponse> getAllUser() {
+        return userService.getAllUser();
     }
 
     @PatchMapping("/users/{id}")
-    public ResponseEntity<UserResponse> updateUser(@Valid @RequestBody UserRequest userRequest) {
-
-        return  ResponseEntity.ok(userService.updateUser(userRequest));
+    public UserResponse updateUser(@Valid @RequestBody UserRequest userRequest) {
+        return userService.updateUser(userRequest);
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<UserResponse> deleteUser(@PathVariable Long id) {
-
-        return ResponseEntity.ok(userService.deleteUser(id));
+    public UserResponse deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
     }
 }
--- src/main/java/com/hackathon/bobFriend/users/service/UserService.java
+++ src/main/java/com/hackathon/bobFriend/users/service/UserService.java
@@ -1,13 +1,10 @@
 package com.hackathon.bobFriend.users.service;
 
-import com.hackathon.bobFriend.common.JwtProvider;
 import com.hackathon.bobFriend.users.dto.UserRequest;
 import com.hackathon.bobFriend.users.dto.UserResponse;
 import com.hackathon.bobFriend.users.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,29 +14,13 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
 
     // 회원 가입
     public UserResponse createUser(UserRequest userRequest) {
 
-        var entity = UserRequest.toEntity(userRequest);
-        entity.setPassword(passwordEncoder.encode(entity.getPassword()));
+        var entity = userRepository.save(UserRequest.toEntity(userRequest));
 
-        return UserResponse.toDto(userRepository.save(entity));
-    }
-
-    // 로그인
-    public String login(UserRequest userRequest) {
-
-        var entity = userRepository.findByEmail(userRequest.getEmail())
-                .orElseThrow(() -> new EntityNotFoundException(String.format("User with email : [%s] not found", userRequest.getEmail())));
-
-        if(!passwordEncoder.matches(userRequest.getPassword(), entity.getPassword())) {
-            throw new BadCredentialsException("invalid password");
-        }
-
-        return jwtProvider.createToken(userRequest.getEmail());
+        return UserResponse.toDto(entity);
     }
 
     // 마이페이지
@@ -64,23 +45,27 @@ public class UserService {
     // 마이페이지 - 회원 정보 수정
     public UserResponse updateUser(UserRequest userRequest) {
 
-        var entity = userRepository.findByEmail(userRequest.getEmail())
-                .orElseThrow(() -> new EntityNotFoundException(String.format("User with email : [%s] not found", userRequest.getEmail())));
+        var entity = userRepository.findByEmail(userRequest.getEmail());
 
-            entity.setName(userRequest.getName());
-            entity.setEmail(userRequest.getEmail());
-            entity.setPassword(passwordEncoder.encode(userRequest.getPassword()));
-
-            return UserResponse.toDto(userRepository.save(entity));
+        if(entity.isPresent()) {
+            var updated = userRepository.save(UserRequest.toEntity(userRequest));
+            return UserResponse.toDto(updated);
+        } else {
+            throw new EntityNotFoundException(userRequest.getEmail());
+        }
     }
 
     // 마이페이지 - 회원 탈퇴
     public UserResponse deleteUser(Long id) {
 
-        var entity = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("User with id : [%s] not found", id)));
+        var entity = userRepository.findById(id);
 
-        entity.setIsActive(false);
-        return UserResponse.toDto(userRepository.save(entity));
+        if(entity.isPresent()) {
+            var found = entity.get();
+            found.setIsActive(false);
+            return UserResponse.toDto(userRepository.save(found));
+        } else {
+            throw new EntityNotFoundException(String.format("User with id : [%s] not found", id));
+        }
     }
 }
