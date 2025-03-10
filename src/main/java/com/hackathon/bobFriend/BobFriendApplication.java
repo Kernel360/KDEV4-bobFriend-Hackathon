4431666d307dcf8064648c104eb99f481b3c59ed
EOJIN <feshed528@gmail.com>
2025-03-10 15:59:28 +0900

    init: 스프링부트 프로젝트 초기 환경설정

---
+++ src/main/java/com/hackathon/bobFriend/BobFriendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bobFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/bobFriend/BobFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.bobFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
